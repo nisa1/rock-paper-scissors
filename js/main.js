45f03f557b48f3f1644a2ef98664573688b73872
nisa <nisa.andrews@gmail.com>
2016-03-21 10:23:18 -0700

    added pseudocode

--- js/main.js
+++ js/main.js
@@ -20,18 +20,20 @@ console.log(computerChoice);
 
 function getResult(p1, p2) {
 
+// Key/Logic for Rock, Paper, Scissors 
   if (p1 === p2) { return 'TIE'; }
   if ((p1 === 'scissors' && p2 === 'rock') || (p1 === 'rock' && p2 === 'paper') || (p1 === 'paper' && p2 === 'scissors')) { return 'LOSS'; }
   else {  return 'WIN'; }
 }
 
+// Show choices
 $(document).ready(function() {
 	$('.play-button').click(function() { 
 		$('.game-container').show();
 	});
 });
 
-
+// When clicking on rock, have computer make a random choice
 $( ".rock" ).click(function() {
   $( "getRandomInt" ).click();
 });
